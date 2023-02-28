4582faca5da26b018906c0c967143e14c51eecdc
Prateek Kasaudhan <prateekkasaudhan123@gmail.com>
2023-02-28 21:39:48 +0530

    Added loader and modified website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import LoadingBar from 'react-top-loading-bar'
 
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
@@ -10,27 +11,34 @@ import {
 } from "react-router-dom";
 
 export default class App extends Component {
+  pageSize=6;
+  apiKey= process.env.REACT_APP_NEWS_API;
+  state = {
+    progress: 0
+  }
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
   render() {
     return (
       <div>
         <Router>
           <NavBar/>
+          <LoadingBar
+          color='#f11946'
+          height= {2}
+          progress={this.state.progress}  />
           <Routes>
             {/* <Route path="/"><News pageSize={5} country="in" category="general" element={</>}/></Route> */}
-            <Route exact path="/" element={<News key="general" pageSize={5} country="in" category="general" />}/>
-            <Route exact path="/business" element={<News key="business" pageSize={5} country="in" category="business" />}/>
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={5} country="in" category="entertainment" />}/>
-            <Route exact path="/general" element={<News key="general" pageSize={5} country="in" category="general" />}/>
-            <Route exact path="/health" element={<News key="health" pageSize={5} country="in" category="health" />}/>
-            <Route exact path="/science" element={<News key="science" pageSize={5} country="in" category="science" />}/>
-            <Route exact path="/sports" element={<News key="sports" pageSize={5} country="in" category="sports" />}/>
-            <Route exact path="/technology" element={<News key="technology" pageSize={5} country="in" category="technology" />}/>
-            {/* <Route path="/entertainment"><News pageSize={5} country="in" category="entertainment" element={<E/>}/></Route>
-            <Route path="/general"><News pageSize={5} country="in" category="general" element={<Home/>}/></Route>
-            <Route path="/health"><News pageSize={5} country="in" category="health" element={<Home/>}/></Route>
-            <Route path="/science"><News pageSize={5} country="in" category="science" element={<Home/>}/></Route>
-            <Route path="/sports"><News pageSize={5} country="in" category="sports" element={<Home/>}/></Route>
-            <Route path="/technology"><News pageSize={5} country="in" category="technology" element={<Home/>}/></Route> */}
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={6} country="in" category="general" />}/>
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={6} country="in" category="business" />}/>
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={6} country="in" category="entertainment" />}/>
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={6} country="in" category="general" />}/>
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={6} country="in" category="health" />}/>
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={6} country="in" category="science" />}/>
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={6} country="in" category="sports" />}/>
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={6} country="in" category="technology" />}/>
+            {/* <Route path="/entertainment"><News pageSize={5} country="in" category="entertainment" element={<E/>}/></Route> */}
           </Routes>
         </Router>
       </div>
